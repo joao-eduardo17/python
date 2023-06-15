8898fe7f96eda29e6a801490b4deef1dc7b591c7
joao-eduardo17 <125677846+joao-eduardo17@users.noreply.github.com>
2023-06-15 08:28:59 -0300

    Adição das listas de 11 a 14

---
+++ Listas/Lista XII.py
@@ -0,0 +1,131 @@
+# Copyright 2010 Google Inc.
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Google's Python Class
+# http://code.google.com/edu/languages/google-python-class/
+
+# Exercícios extras
+
+# G. verbing
+# Dada uma string, caso seu comprimento seja pelo menos 3,
+# adiciona 'ing' no final
+# Caso a string já termine em 'ing', acrescentará 'ly'.
+def verbing(s):
+  if len(s) < 3:
+    return s
+  elif s[-3]+s[-2]+s[-1] == 'ing':
+    return s+'ly'
+  return s+ 'ing'
+  
+# H. not_bad
+# Dada uma string, procura a primeira ocorrência de 'not' e 'bad'
+# Se 'bad' aparece depois de 'not' troca 'not' ... 'bad' por 'good'
+# Assim 'This dinner is not that bad!' retorna 'This dinner is good!'
+def not_bad(s):
+  if s.find('bad') > s.find('not'):
+    return s[:s.find('not')] + 'good' + s[s.find('bad')+3:]
+  return s
+
+# I. inicio_final
+# Divida cada string em dois pedaços.
+# Se a string tiver um número ímpar de caracteres
+# o primeiro pedaço terá um caracter a mais,
+# Exemplo: 'abcde', divide-se em 'abc' e 'de'.
+# Dadas 2 strings, a e b, retorna a string
+# a_inicio + b_inicio + a_final + b_final
+def inicio_final(a, b):
+  metadeA, metadeB = len(a)//2, len(b)//2
+  if len(a) % 2 == 0:
+    a_inicio = a[0:metadeA]
+    a_final = a[metadeA:]
+  else:
+    a_inicio = a[0:metadeA+1]
+    a_final = a[metadeA+1:]
+  if len(b) % 2 == 0:
+    b_inicio = b[0:metadeB]
+    b_final = b[metadeB:]
+  else:
+    b_inicio = b[0:metadeB+1]
+    b_final = b[metadeB+1:]  
+  return a_inicio + b_inicio + a_final + b_final
+
+# J. zeros finais
+# Verifique quantos zeros há no final de um número inteiro positivo
+# Exemplo: 10010 tem 1 zero no fim e 908007000 possui três
+def zf(n):
+  maior = 0
+  for c in str(n):
+    if c == '0':
+      maior+= 1
+    else:
+      maior = 0
+  return maior
+
+
+# K. conta 2
+# Verifique quantas vezes o dígito 2 aparece entre 0 e n-1
+# Exemplo: para n = 20 o dígito 2 aparece duas vezes entre 0 e 19
+def conta2(n):
+  x = ''
+  for c in range(0,n):
+    if '2' in str(c):
+      x+= str(c)
+  return x.count('2')
+
+# L. inicio em potencia de 2
+# Dado um número inteiro positivo n retorne a primeira potência de 2
+# que tenha o início igual a n
+# Exemplo: para n = 65 retornará 16 pois 2**16 = 65536
+def inip2(n):
+  x = 0
+  while n:
+    if str(2**x)[0:len(str(n))] == str(n):
+      return x
+    x+=1
+
+def test(obtido, esperado):
+  if obtido == esperado:
+    prefixo = ' Parabéns!'
+  else:
+    prefixo = ' Ainda não'
+  print ('%s obtido: %s esperado: %s' % (prefixo, repr(obtido), repr(esperado)))
+
+def main():
+  print ('verbing')
+  test(verbing('hail'), 'hailing')
+  test(verbing('swiming'), 'swimingly')
+  test(verbing('do'), 'do')
+
+  print ()
+  print ('not_bad')
+  test(not_bad('This movie is not so bad'), 'This movie is good')
+  test(not_bad('This dinner is not that bad!'), 'This dinner is good!')
+  test(not_bad('This tea is not hot'), 'This tea is not hot')
+  test(not_bad("It's bad yet not"), "It's bad yet not")
+
+  print ()
+  print ('inicio_final')
+  test(inicio_final('abcd', 'xy'), 'abxcdy')
+  test(inicio_final('abcde', 'xyz'), 'abcxydez')
+  test(inicio_final('Kitten', 'Donut'), 'KitDontenut')
+
+  print ()
+  print ('zeros finais')
+  test(zf(10100100010000), 4)
+  test(zf(90000000000000000010), 1)
+
+  print ()
+  print ('conta 2')
+  test(conta2(20), 2)
+  test(conta2(999), 300)
+  test(conta2(555), 216)
+
+  print ()
+  print ('inicio p2')
+  test(inip2(7), 46)
+  test(inip2(133), 316)
+  test(inip2(1024), 10)
+  
+if __name__ == '__main__':
+  main()
