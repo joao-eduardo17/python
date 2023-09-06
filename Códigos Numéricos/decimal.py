bc77bb5ab5b70322c93f6a8f647bc0974e8d2967
joao-eduardo17 <125677846+joao-eduardo17@users.noreply.github.com>
2023-09-06 11:47:00 -0300

    Update decimal.py
    adição da conversão do sistema Decimal para o sistema Hexadecimal

--- Códigos Numéricos/decimal.py
+++ Códigos Numéricos/decimal.py
@@ -8,7 +8,7 @@ def binário(n):
         n = n//2
     return int('1' + resultado[::-1])
 
-#Deciaml para octal
+#Decimal para octal
 def octal(n):
     resultado = ''
     if n <= 8:
@@ -20,13 +20,35 @@ def octal(n):
     return int(x)
 
 #Decimal para hexadecimal
-# def hexa(n):
-#     resultado = ''
-#     if n <= 16:
-#         return n
-#     while n > 16:
-#         x = n % 16
-#         if x > 9:
-#             match (x):
-#                 case 11:
-#                      return
+def hexa(n):
+    resultado = ''
+    letras = 'a b c d e f'.split()
+    index = 10
+    if n <= 9:
+        return n
+    if n > 9 and n <= 15:
+        for c in range(6):
+            if n == index:
+
+                print(letras[c])
+                return
+            index += 1
+    while n > 15:
+        x = n % 16
+        if x > 9:
+            index = 10
+            for k in range(6):
+                if x == index:
+                    resultado += letras[k]
+                index += 1
+        else:
+            resultado += str(x)
+        n = n//16
+    if n > 9:
+        index = 10
+        for p in range(6):
+            if n == index:
+                n = letras[p]
+            index += 1
+    print(str(n) + resultado[::-1])
+    return
