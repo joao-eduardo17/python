7b2af75c8b130cee7083845397799241e9cc7c6d
joao-eduardo17 <125677846+joao-eduardo17@users.noreply.github.com>
2023-09-08 14:11:54 -0300

    término do Sistema binário
    Agora o sistema binário pode ser transformado em outros três sistemas numéricos

--- Códigos Numéricos/binario.py
+++ Códigos Numéricos/binario.py
@@ -1,14 +1,22 @@
 #Binário para octal
 def octal(n):
-    invertido = str(n)[::-1]
+    n = str(n)
     resultado = ''
     numeros = '000 001 010 011 100 101 110 111'.split()
-    for c in range(len(invertido)):
+    match(len(n)%3):
+        case 1:
+            n = '00' + n
+        case 2:
+            n = '0' + n
+    for c in range(0, len(n), 3):
+        contador = 0
+        index = n[c:c+3]
         for k in numeros:
-            print(str(invertido[c:c+4]))
-            if str(invertido[c:c+4]) == k:
-                resultado += k
-    return resultado[::-1]
+            if index == k:
+                resultado += str(contador)
+                break
+            contador += 1
+    return resultado
 
 #Binário para decimal
 def decimal(n):
@@ -22,4 +30,34 @@ def decimal(n):
 
 #Binário para hexadecimal
 def hexa(n):
-    return
+    n = str(n)
+    resultado = ''
+    letras = 'a b c d e f'.split()
+    numeros = '0000 0001 0010 0011 0100 0101 0110 0111 1000 1001 1010 1011 1100 1101 1110 1111'.split()
+    match(len(n) % 4):
+        case 1:
+            n = '000' + n
+        case 2:
+            n = '00' + n
+        case 3:
+            n = '0' + n
+    for c in range(0,len(n), 4):
+        contador = 0
+        index = n[c:c+4]
+        for k in numeros:
+            if index == k and contador > 9:
+                cont2 = 10
+                for p in letras:
+                    if contador == cont2:
+                        resultado += p
+                        break
+                    cont2 += 1
+            elif index == k:
+                resultado += str(contador)
+                break
+            contador += 1
+    return resultado
+                    
+        
+   
+    
