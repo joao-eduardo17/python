c5ac4ecea5aa3ce0aa9554090a5fb161bb82bc56
joao-eduardo17 <125677846+joao-eduardo17@users.noreply.github.com>
2023-09-12 20:00:24 -0300

    mais adições
    adições do octal para o hexadecimal e do hexadecimal para o binario

--- Códigos Numéricos/Hexadecimal.py
+++ Códigos Numéricos/Hexadecimal.py
@@ -1,6 +1,29 @@
 #Hexadecimal para binário
 def binario(n):
-    return
+    resultado = ''
+    n = str(n).lower()
+    numeros = ['0000', '0001' ,'0010', '0011', '0100', '0101', '0110', '0111', '1000', '1001', '1010', '1011', '1100', '1101', '1110', '1111']
+    letras = ['a','b','c','d','e','f']
+    for c in n:
+        index = 0
+        confirma = False
+        while index < 16:
+            if index > 9:
+                contador = 0
+                while contador < 6:
+                    if c == letras[contador]:
+                        resultado += numeros[index]
+                        confirma = True
+                        break
+                    contador += 1
+                    index += 1
+            elif str(index) == c:
+                resultado += numeros[index]
+                break
+            if confirma:
+                break
+            index +=1
+    return resultado
 
 #Hexadecimal para octal
 def octal(n):
--- Códigos Numéricos/Octal.py
+++ Códigos Numéricos/Octal.py
@@ -21,4 +21,39 @@ def decimal(n):
 
 #Octal para Hexadecimal
 def hexa(n):
+    n = str(n)
+    resultado = ''
+    numeros = '000 001 010 011 100 101 110 111'.split()
+    for c in n:
+        for contador in range(len(numeros)):
+            if c == str(contador):
+                resultado += numeros[contador]
+                break
+    n = str(resultado)
+    resultado = ''
+    letras = 'a b c d e f'.split()
+    numeros = '0000 0001 0010 0011 0100 0101 0110 0111 1000 1001 1010 1011 1100 1101 1110 1111'.split()
+    match(len(n) % 4):
+        case 1:
+            n = '000' + n
+        case 2:
+            n = '00' + n
+        case 3:
+            n = '0' + n
+    for c in range(0,len(n), 4):
+        contador = 0
+        index = n[c:c+4]
+        for k in numeros:
+            if index == k and contador > 9:
+                cont2 = 10
+                for p in letras:
+                    if contador == cont2:
+                        resultado += p
+                        break
+                    cont2 += 1
+            elif index == k:
+                resultado += str(contador)
+                break
+            contador += 1
+    return resultado
     
