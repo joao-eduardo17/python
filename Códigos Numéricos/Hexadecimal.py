59305d76e7a08c3c152d814dbadfb6543205075f
joao-eduardo17 <125677846+joao-eduardo17@users.noreply.github.com>
2023-09-13 12:13:23 -0300

    Update Hexadecimal.py
    Término das conversões em Hexadecimal para Decimal e Binário

--- Códigos Numéricos/Hexadecimal.py
+++ Códigos Numéricos/Hexadecimal.py
@@ -27,8 +27,73 @@ def binario(n):
 
 #Hexadecimal para octal
 def octal(n):
-    return
+    resultado = ''
+    n = str(n).lower()
+    numeros = ['0000', '0001' ,'0010', '0011', '0100', '0101', '0110', '0111', '1000', '1001', '1010', '1011', '1100', '1101', '1110', '1111']
+    letras = ['a','b','c','d','e','f']
+    for c in n:
+        index = 0
+        confirma = False
+        while index < 16:
+            if index > 9:
+                contador = 0
+                while contador < 6:
+                    if c == letras[contador]:
+                        resultado += numeros[index]
+                        confirma = True
+                        break
+                    contador += 1
+                    index += 1
+            elif str(index) == c:
+                resultado += numeros[index]
+                break
+            if confirma:
+                break
+            index +=1
+    n = str(int(resultado))
+    resultado = ''
+    numeros = '000 001 010 011 100 101 110 111'.split()
+    match(len(n)%3):
+        case 1:
+            n = '00' + n
+        case 2:
+            n = '0' + n
+    for c in range(0, len(n), 3):
+        contador = 0
+        index = n[c:c+3]
+        for k in numeros:
+            if index == k:
+                resultado += str(contador)
+                break
+            contador += 1
+    return int(resultado)
 
+    
 #Hexadecimal para decimal
 def decimal(n):
-    return
+    letras = ['a','b','c','d','e','f']
+    n = str(n)
+    resultado = 0
+    elevado = len(n) - 1
+    for c in n:
+        index = 0
+        confirma = False
+        while index < 16:
+            if index > 9:
+                contador = 0
+                while contador < 6:
+                    if c == letras[contador]:
+                        resultado += (index) * (16**elevado)
+                        elevado -= 1
+                        confirma = True
+                        break
+                    contador += 1
+                    index += 1
+            elif c == str(index):
+                resultado += (index) * (16**elevado)
+                elevado -= 1
+                break
+            if confirma:
+                break
+            index += 1
+    return resultado
